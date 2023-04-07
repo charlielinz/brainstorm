240d80d9a88c73bfa01c67f43816608a66cb1075
charlielinz <ilovealinlin@gmail.com>
2023-04-07 21:01:40 +0800

    finish mvp

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-zinc-900 text-gray-100">
+      <body className="text-zinc-900 bg-gray-200">
         <Main />
         <NextScript />
       </body>
--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,83 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   const [nameInput, setNameInput] = useState("");
+  const [recipientInput, setRecipientInput] = useState("");
   const [relationshipInput, setRelationshipInput] = useState("");
   const [descriptionInput, setDescriptionInput] = useState("");
   const [results, setResults] = useState([]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-          name: nameInput,
-          relationship: relationshipInput,
-          description: descriptionInput,
-        }),
-      });
-
-      const data = await response.json();
-      console.log(data.result);
-      if (response.status !== 200) {
-        throw (
-          data.error ||
-          new Error(`Request failed with status ${response.status}`)
-        );
-      }
-
-      setResults(parser(data.result));
-      console.log(results);
-      setNameInput("");
-      setRelationshipInput("");
-      setDescriptionInput("");
-    } catch (error) {
-      console.error(error);
-      alert(error.message);
+    setResults("");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        name: nameInput,
+        recipient: recipientInput,
+        relationship: relationshipInput,
+        description: descriptionInput,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
     }
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+
+    let done = false;
+    let count = 0;
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      if (chunkValue === "\n") {
+        setResults((prev) => [...prev, <br key={count} />]);
+      } else {
+        setResults((prev) => [...prev, chunkValue]);
+      }
+      count += 1;
+    }
+    setNameInput("");
+    setRecipientInput("")
+    setRelationshipInput("");
+    setDescriptionInput("");
   };
   return (
     <div className="flex flex-col">
-      <div className="max-w-2xl w-full mx-auto bg-gradient-to-br from-green-200 to-green-700 rounded">
+      <div className="max-w-2xl w-full mx-auto bg-gradient-to-br from-gray-300 to-gray-700 rounded">
         <form
-          className="flex flex-col items-center bg-zinc-800 m-1 pt-2 rounded-lg"
+          className="flex flex-col items-center bg-gray-100 m-1 pt-2 rounded-lg"
           onSubmit={handleSubmit}
         >
           <div className="flex flex-col w-full px-4">
             <input
-              className="bg-zinc-700 px-3 py-1 outline-0 rounded-t"
+              className="bg-gray-200 px-3 py-1 outline-0 rounded-t"
               type="text"
               name="name"
-              placeholder="his/her name"
+              placeholder="my name is"
               value={nameInput}
               autoComplete="off"
               onChange={(e) => setNameInput(e.target.value)}
             />
             <input
-              className="bg-zinc-700 px-3 py-1 outline-0"
+              className="bg-gray-200 px-3 py-1 outline-0"
+              type="text"
+              name="recipientInput"
+              placeholder="write this to"
+              value={recipientInput}
+              autoComplete="off"
+              onChange={(e) => setRecipientInput(e.target.value)}
+            />
+            <input
+              className="bg-gray-200 px-3 py-1 outline-0"
               type="text"
               name="relationship"
               placeholder="he/she is my"
@@ -67,18 +86,18 @@ const Home = () => {
               onChange={(e) => setRelationshipInput(e.target.value)}
             />
             <textarea
-              className="bg-zinc-700 px-3 py-1 outline-0 h-24 rounded-b"
+              className="bg-gray-200 px-3 py-1 outline-0 h-24 rounded-b"
               style={{ resize: "none" }}
               type="textarea"
               name="description"
-              placeholder="I wanna tell him/her"
+              placeholder="describe the thing"
               value={descriptionInput}
               autoComplete="off"
               onChange={(e) => setDescriptionInput(e.target.value)}
             />
           </div>
           <button
-            className="border-[0.5px] outline-0 w-48 my-2 p-1 rounded"
+            className="border-[0.5px] outline-0 w-48 my-2 p-1 rounded bg-zinc-800 text-gray-200"
             type="submit"
           >
             Generate Email
@@ -88,17 +107,6 @@ const Home = () => {
       <div>{results}</div>
     </div>
   );
-};
-
-const parser = (result) => {
-  const lines = result.split("\n");
-  return lines.map((line, index) => {
-    if (line === "") {
-      return <br key={index} />;
-    } else {
-      return <p key={index}>{line}</p>;
-    }
-  });
 };
 
 export default Home;
